cbf13537540b30fa30694c94c5b2cfcb6e0f0967
stalg94 <76990526+stalg94@users.noreply.github.com>
2021-10-25 21:22:10 +0300

    setup

---
+++ AppointmentSchedulling/AppointmentSchedulling/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppointmentSchedulling.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
